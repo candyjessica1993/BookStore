97fc0d86d6a99ce8ce65b7ce0febf4da8b4ba293
Le Phuoc <a1504524@myy.haaga-helia.fi>
2016-09-09 15:13:47 +0300

    First commit

---
+++ src/main/java/Bookstore/web/BookController.java
@@ -0,0 +1,21 @@
+package Bookstore.web;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+
+@Controller
+
+public class BookController {
+	@RequestMapping(value="/index", method=RequestMethod.GET)
+
+	public String BookList(Model model) {
+
+		return "hello";
+	}
+
+}
